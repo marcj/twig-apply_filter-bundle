cb0f383d24bd8e80c960070c3a11807d719a8885
Marc J. Schmidt <marc@kryn.org>
2013-12-12 14:45:15 +0100

    Fixed wrong namespace.

--- DependencyInjection/MJSTwigApplyFilterExtension.php
+++ DependencyInjection/MJSTwigApplyFilterExtension.php
@@ -2,7 +2,6 @@
 
 namespace MJS\TwigApplyFilter\DependencyInjection;
 
-use FOS\RestBundle\DependencyInjection\Configuration;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
@@ -15,9 +14,6 @@ class MJSTwigApplyFilterExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
